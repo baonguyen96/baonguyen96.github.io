aacb96befb6e55ccf1fa52b99fe5915f71c6d3ab
baonguyen96 <baonguyendrump@gmail.com>
2019-04-30 09:30:36 -0500

    Reroute 404

---
+++ scripts/functionalities/resolve404.js
@@ -0,0 +1,18 @@
+$(function() {
+	let link404 = $("#link404");
+
+	link404.click(function(){
+		let currentURL = window.location.href;
+		let newURL = '';
+
+		if(currentURL.includes("localhost")) {
+			newURL = 'http://localhost:63343/baonguyen96.github.io';
+		}
+		else {
+			newURL = 'https://baonguyen96.github.io/';
+		}
+
+		window.location.replace(newURL);
+	});
+
+});
