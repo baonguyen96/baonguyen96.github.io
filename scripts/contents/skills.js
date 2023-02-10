b8076c4d85ee7aec4f189368674b808443537311
baonguyen96 <baonguyendrump@gmail.com>
2023-02-10 07:36:24 -0600

    Change experience column wrapping

--- scripts/contents/skills.js
+++ scripts/contents/skills.js
@@ -8,7 +8,15 @@ let skills = [
         "value": "Azure, AWS, .NET, J2EE, Docker/Kubernetes"
     },
     {
-        "name": "Web Technologies",
+        "name": "Infrastructure",
+        "value": "CloudFormation, Terraform, Sceptre"
+    },
+    {
+        "name": "Data Engineering",
+        "value": "Data Lake, Data Warehouse, SSIS/SSDT, Apache Spark, EMR, Glue, Redshift, LakeFormation"
+    },
+    {
+        "name": "Web",
         "value": "SpringBoot, HTML, CSS, JavaScript/jQuery, Bootstrap, XML, WordPress, REST, SOAP, RDF, Google Analytics"
     },
     {
@@ -20,21 +28,12 @@ let skills = [
         "value": "JUnit, TestNG, MSUnitTest, Python UnitTest, Mocking, Selenium WebDriver API"
     },
     {
-        "name": "Data Engineering",
-        "value": "Data Lake, Data Warehouse, SSIS/SSDT, Apache Spark, EMR, Glue"
-    },
-    {
         "name": "CI/CD",
-        "value": "Jenkins, TeamCity, Maven, Gradle, Ant, Git, GitHub, BitBucket"
+        "value": "Jenkins, TeamCity, Maven, Gradle, Ant, Conda, Git, GitHub, BitBucket"
     },
     {
         "name": "Operating Systems",
         "value": "Windows, Linux, MacOS, Android"
-    },
-    {
-        "name": "Development Tools",
-        "value": "JetBrains IDEs, Eclipse, Visual Studio, Android Studio, Microsoft SSMS, MySQL Workbench, " +
-            "Microsoft Office suite, SharePoint, Adobe Creative Cloud suite, RStudio, Postman, SwaggerUI, PowerBI"
     },
     {
         "name": "Project Management",
@@ -43,6 +42,11 @@ let skills = [
     {
         "name": "Development Methodologies",
         "value": "Agile/SCRUM, Test Driven Development, Mobile-First Development"
+    },
+    {
+        "name": "Development Tools",
+        "value": "JetBrains IDEs, Eclipse, Visual Studio, Android Studio, Microsoft SSMS, MySQL Workbench, " +
+            "Microsoft Office suite, SharePoint, Adobe Creative Cloud suite, RStudio, Postman, SwaggerUI, PowerBI"
     },
     {
         "name": "Miscellaneous",
